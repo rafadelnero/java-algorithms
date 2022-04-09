26c7396ac64ccd8dd23c8c7e5ceb04ec69c2caf9
rafaeldelnero <rafael.delnero@fabfitfun.com>
2022-04-09 14:38:41 +0100

    - Added more algorithms.

--- src/main/java/algoexpert/easy/BreadthFirstSearch.java
+++ src/main/java/algoexpert/easy/BreadthFirstSearch.java
@@ -4,10 +4,6 @@ import java.util.*;
 
 public class BreadthFirstSearch {
 
-    // Do not edit the class below except
-    // for the breadthFirstSearch method.
-    // Feel free to add new properties
-    // and methods to the class.
     static class Node {
         String name;
         List<Node> children = new ArrayList<>();
--- src/main/java/algoexpert/medium/SpiralTraverse.java
+++ src/main/java/algoexpert/medium/SpiralTraverse.java
@@ -9,29 +9,75 @@ import java.util.List;
 public class SpiralTraverse {
 
     public static List<Integer> spiralTraverse(int[][] array) {
-        List<Integer> spiralTraverse = new ArrayList<>();
+        if (array.length == 0) return new ArrayList<>();
 
-        var startingRow = 0;
-        var startingColumn = 0;
-        var endingColumn = 0;
-        var endingRow = 0;
+        List<Integer> result = new ArrayList<>();
+        var startRow = 0;
+        var endRow = array.length - 1;
+        var startCol = 0;
+        var endCol = array[0].length - 1;
 
-        var spiralSize = array.length - 1;
+        while (startRow <= endRow && startCol <= endCol) {
+            for (int col = startCol; col <= endCol; col++) {
+                result.add(array[startRow][col]);
+            }
 
-        for (; startingColumn < spiralSize; startingColumn++) {
-            spiralTraverse.add(array[startingRow][startingColumn]);
+            for (int row = startRow + 1; row <= endRow; row++) {
+                result.add(array[row][endCol]);
+            }
+
+            for (int col = endCol - 1; col >= startCol; col--) {
+                if (startRow == endRow) break;
+                result.add(array[endRow][col]);
+            }
+
+            for (int row = endRow - 1; row > startRow; row--) {
+                if (startCol == endCol) break;
+                result.add(array[row][startCol]);
+            }
+
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
         }
 
-        for (int i = startingColumn; i < spiralSize; i++) {
-            spiralTraverse.add(array[++startingRow][startingColumn]);
+        return result;
+    }
+
+    public static List<Integer> spiralTraverseRecursive(int[][] array) {
+        if (array.length == 0) return new ArrayList<>();
+
+        var result = new ArrayList<Integer>();
+        spiralFill(array, 0, array.length - 1, 0, array[0].length - 1, result);
+        return result;
+    }
+
+    public static void spiralFill(int [][] array, int startRow, int endRow,
+                                  int startCol, int endCol, List<Integer> result) {
+        if (startRow > endRow || startCol > endCol) {
+            return;
         }
 
-        for (int i = startingColumn; i < spiralSize; i++) {
-            spiralTraverse.add(array[startingRow][startingColumn--]);
+        for (int col = startCol; col <= endCol; col++) {
+            result.add(array[startRow][col]);
         }
 
+        for (int row = startRow + 1; row <= endRow; row++) {
+            result.add(array[row][endCol]);
+        }
 
-        return spiralTraverse;
+        for (int col = endCol - 1; col >= startCol; col--) {
+            if (startRow == endRow) break;
+            result.add(array[endRow][col]);
+        }
+
+        for (int row = endRow - 1; row > startRow; row--) {
+            if (startCol == endCol) break;
+            result.add(array[row][startCol]);
+        }
+
+        spiralFill(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result);
     }
 
     @Test
@@ -44,6 +90,17 @@ public class SpiralTraverse {
                         {10, 9, 8, 7},
                 };
         var expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+        var actual = spiralTraverseRecursive(input);
+        System.out.println(actual);
+    }
+
+    @Test
+    public void TestCase2() {
+        var input =
+                new int[][] {
+                        {1},
+                };
+        var expected = Arrays.asList(1);
         var actual = spiralTraverse(input);
         System.out.println(actual);
     }
